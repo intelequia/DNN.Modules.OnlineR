bce744d4a4ab6d1611f4859a03567b6a52a0e8e5
Víctor Jordan Calero Méndez <victorjordan.cm@hotmail.com>
2015-05-18 15:26:25 +0100

    Release Commit
    Version 00.01.00

---
+++ Components/UsersOnLine.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Christoc.Modules.OnlineR.Components
+{
+
+    public class UsersOnLine : Hub                           // Hub definition
+    {
+        static List<string> users=new List<string>();
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            string clientId = GetClientId();
+   
+            if (users.IndexOf(clientId)==-1) {               // When a user connect to the page we search in the list his client id
+                users.Add(clientId);                         // If not exist in the list we add his id 
+            }
+
+            ShowUsersOnLine();
+
+            return base.OnConnected();
+        }
+
+        public override System.Threading.Tasks.Task OnReconnected()
+        {
+            string clientId = GetClientId();
+            if (users.IndexOf(clientId) == -1)             // Same as before
+            {
+                users.Add(clientId);
+            }
+
+            ShowUsersOnLine();
+        
+            return base.OnReconnected();
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            string clientId = GetClientId();
+            
+            if (users.IndexOf(clientId) > -1)            // When the user disconects if exist in the list 
+            {
+                users.Remove(clientId);                  // Then remove it
+            }
+
+            ShowUsersOnLine();
+
+            return base.OnDisconnected(stopCalled);
+        }
+
+
+        private string GetClientId() {                   // Method that returns the id of the client
+            string clientId = "";
+            if (!(Context.QueryString["clientId"] == null))
+            {
+                clientId = Context.QueryString["clientId"].ToString();
+            }
+
+            if (string.IsNullOrEmpty(clientId.Trim()))
+            {
+                clientId = Context.ConnectionId;
+            }
+
+            return clientId;
+        
+        }
+
+        public void ShowUsersOnLine()                 // Send the list count to all clients
+        {
+            Clients.All.showUsersOnLine(users.Count);
+        }
+    }
+}
+
+  
+
---
+++ Settings.ascx
@@ -0,0 +1,33 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Settings.ascx.cs" Inherits="Christoc.Modules.OnlineR.Settings" %>
+
+
+<!-- uncomment the code below to start using the DNN Form pattern to create and update settings -->
+ 
+
+<%@ Register TagName="label" TagPrefix="dnn" Src="~/controls/labelcontrol.ascx" %>
+
+	<h2 id="dnnSitePanel-BasicSettings" class="dnnFormSectionHead"><a href="" class="dnnSectionExpanded"><%=LocalizeString("BasicSettings")%></a></h2>
+	<fieldset>
+        <div class="dnnFormItem">
+            <dnn:Label ID="lblTitle" runat="server" /> 
+            <asp:TextBox ID="txtTitle" runat="server"></asp:TextBox>
+        </div>
+    </fieldset>
+    <fieldset>
+        <div class="dnnFormItem">
+            <dnn:Label ID="lblPrimaryColor" runat="server" />
+            <asp:TextBox ID="txtPrimaryColor" runat="server"></asp:TextBox>
+        </div>
+    </fieldset>
+    <fieldset>
+    <div class="dnnFormItem">
+        <dnn:Label ID="lblSelectedSkin" runat="server" />
+        <asp:DropDownList runat="server" ID="ddlSelectedSkin" DataTextField="skinName" DataValueField="skinFile">
+            <asp:ListItem Value="Material.css">Material</asp:ListItem>
+            <asp:ListItem Value="Tile.css">Tile</asp:ListItem>
+            <asp:ListItem Value="Social.css">Social</asp:ListItem>
+            <asp:ListItem Value="Minimalist.css">Minimalist</asp:ListItem>
+            <asp:ListItem Value="OnlyNumber.css">OnlyNumber</asp:ListItem>
+        </asp:DropDownList>
+    </div>
+    </fieldset>
